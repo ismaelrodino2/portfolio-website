0c33533be6d75fa8055d976020a61f874fc09030
Ismael <ismael.rodino@hotmail.com>
2023-11-29 18:36:36 -0300

    fix language not changing, always loading

--- next.config.js
+++ next.config.js
@@ -1,32 +1,6 @@
 const nextTranslate = require('next-translate-plugin')
 
-module.exports = nextTranslate({
-
-
-  future: {
-    strictPostcssConfiguration: true,
-  },
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: securityHeaders,
-      },
-    ];
-  },
-  webpack: (config, { dev, isServer }) => {
-    // Replace React with Preact only in client production build
-    if (!dev && !isServer) {
-      Object.assign(config.resolve.alias, {
-        react: "preact/compat",
-        "react-dom/test-utils": "preact/test-utils",
-        "react-dom": "preact/compat",
-      });
-    }
-
-    return config;
-  },
-})
+module.exports = nextTranslate()
 
 // https://securityheaders.com
 const ContentSecurityPolicy = `
